9ac26ff690150c5d29ecfb1cbeff05e81b702425
SebastianOrtegaM <94182902+SebastianOrtegaM@users.noreply.github.com>
2023-07-20 18:55:43 -0600

    Update Clinica_Dental.sql

--- Clinica_Dental.sql
+++ Clinica_Dental.sql
@@ -29,7 +29,7 @@ CREATE TABLE Seguro (
 CREATE TABLE Usuario (
   id_usuario NUMBER PRIMARY KEY,
   nombre_usuario VARCHAR2(50),
-  contraseña VARCHAR2(100),
+  contraseÃ±a VARCHAR2(100),
   id_rol NUMBER,
   FOREIGN KEY (id_rol) REFERENCES Rol(id_rol)
 );
@@ -66,7 +66,7 @@ CREATE TABLE Registro (
   observaciones VARCHAR2(500),
   FOREIGN KEY (id_paciente) REFERENCES Paciente(id_paciente)
 );
-CREATE TABLE Estadísticas (
+CREATE TABLE EstadÃ­sticas (
   id_estadistica NUMBER PRIMARY KEY,
   id_doctor NUMBER,
   id_tratamiento NUMBER,
@@ -99,35 +99,35 @@ VALUES (1, 'Seguro A', 'Cobertura completa');
 INSERT INTO Seguro (id_seguro, nombre_seguro, tipo_cobertura)
 VALUES (2, 'Seguro B', 'Cobertura parcial');
 INSERT INTO Seguro (id_seguro, nombre_seguro, tipo_cobertura)
-VALUES (3, 'Seguro C', 'Cobertura básica');
+VALUES (3, 'Seguro C', 'Cobertura bÃ¡sica');
 
 INSERT INTO Rol (id_rol, nombre_rol)
 VALUES (1, 'Administrador');
 INSERT INTO Rol (id_rol, nombre_rol)
-VALUES (2, 'Médico');
+VALUES (2, 'MÃ©dico');
 INSERT INTO Rol (id_rol, nombre_rol)
 VALUES (3, 'Paciente');
 
 INSERT INTO Doctor (id_doctor, nombre, especialidad, id_usuario)
-VALUES (1, 'Dr. Juan Pérez', 'Odontología', 1);
+VALUES (1, 'Dr. Juan PÃ©rez', 'OdontologÃ­a', 1);
 INSERT INTO Doctor (id_doctor, nombre, especialidad, id_usuario)
-VALUES (2, 'Dra. María Gómez', 'Ortodoncia', 2);
+VALUES (2, 'Dra. MarÃ­a GÃ³mez', 'Ortodoncia', 2);
 INSERT INTO Doctor (id_doctor, nombre, especialidad, id_usuario)
-VALUES (3, 'Dr. Carlos Rodríguez', 'Endodoncia', 3);
+VALUES (3, 'Dr. Carlos RodrÃ­guez', 'Endodoncia', 3);
 
-INSERT INTO Usuario (id_usuario, nombre_usuario, contraseña, id_rol)
+INSERT INTO Usuario (id_usuario, nombre_usuario, contraseÃ±a, id_rol)
 VALUES (1, 'admin', 'admin123', 1);
-INSERT INTO Usuario (id_usuario, nombre_usuario, contraseña, id_rol)
+INSERT INTO Usuario (id_usuario, nombre_usuario, contraseÃ±a, id_rol)
 VALUES (2, 'medico1', 'pass123', 2);
-INSERT INTO Usuario (id_usuario, nombre_usuario, contraseña, id_rol)
+INSERT INTO Usuario (id_usuario, nombre_usuario, contraseÃ±a, id_rol)
 VALUES (3, 'paciente1', 'user123', 3);
 
 INSERT INTO Paciente (id_paciente, nombre, fecha_nacimiento, id_seguro)
-VALUES (1, 'Ana Martínez', TO_DATE('1990-05-15', 'YYYY-MM-DD'), 1);
+VALUES (1, 'Ana MartÃ­nez', TO_DATE('1990-05-15', 'YYYY-MM-DD'), 1);
 INSERT INTO Paciente (id_paciente, nombre, fecha_nacimiento, id_seguro)
-VALUES (2, 'Pedro Gómez', TO_DATE('1985-10-22', 'YYYY-MM-DD'), 2);
+VALUES (2, 'Pedro GÃ³mez', TO_DATE('1985-10-22', 'YYYY-MM-DD'), 2);
 INSERT INTO Paciente (id_paciente, nombre, fecha_nacimiento, id_seguro)
-VALUES (3, 'María López', TO_DATE('1998-02-28', 'YYYY-MM-DD'), 3);
+VALUES (3, 'MarÃ­a LÃ³pez', TO_DATE('1998-02-28', 'YYYY-MM-DD'), 3);
 
 INSERT INTO Cita (id_cita, fecha, hora, id_doctor, id_paciente)
 VALUES (1, TO_DATE('2023-07-20', 'YYYY-MM-DD'), '09:00', 1, 1);
@@ -139,7 +139,7 @@ VALUES (3, TO_DATE('2023-07-22', 'YYYY-MM-DD'), '14:00', 3, 3);
 INSERT INTO Tratamiento (id_tratamiento, nombre_tratamiento, costo)
 VALUES (1, 'Limpieza dental', 100);
 INSERT INTO Tratamiento (id_tratamiento, nombre_tratamiento, costo)
-VALUES (2, 'Extracción de muela', 200);
+VALUES (2, 'ExtracciÃ³n de muela', 200);
 INSERT INTO Tratamiento (id_tratamiento, nombre_tratamiento, costo)
 VALUES (3, 'Ortodoncia', 500);
 
@@ -160,22 +160,22 @@ VALUES (3, 3, TO_DATE('2023-07-23', 'YYYY-MM-DD'), 500);
 INSERT INTO Reclamo (id_reclamo, id_paciente, descripcion, fecha)
 VALUES (1, 1, 'No estoy satisfecha con el tratamiento recibido.', TO_DATE('2023-07-24', 'YYYY-MM-DD'));
 INSERT INTO Reclamo (id_reclamo, id_paciente, descripcion, fecha)
-VALUES (2, 2, 'Me han cobrado de más en mi última factura.', TO_DATE('2023-07-25', 'YYYY-MM-DD'));
+VALUES (2, 2, 'Me han cobrado de mÃ¡s en mi Ãºltima factura.', TO_DATE('2023-07-25', 'YYYY-MM-DD'));
 INSERT INTO Reclamo (id_reclamo, id_paciente, descripcion, fecha)
-VALUES (3, 3, 'He tenido problemas con el seguro médico.', TO_DATE('2023-07-26', 'YYYY-MM-DD'));
+VALUES (3, 3, 'He tenido problemas con el seguro mÃ©dico.', TO_DATE('2023-07-26', 'YYYY-MM-DD'));
 
 INSERT INTO Registro (id_registro, id_paciente, fecha_registro, observaciones)
 VALUES (1, 1, TO_DATE('2023-07-20', 'YYYY-MM-DD'), 'Limpieza dental realizada.');
 INSERT INTO Registro (id_registro, id_paciente, fecha_registro, observaciones)
-VALUES (2, 2, TO_DATE('2023-07-21', 'YYYY-MM-DD'), 'Extracción de muela realizada.');
+VALUES (2, 2, TO_DATE('2023-07-21', 'YYYY-MM-DD'), 'ExtracciÃ³n de muela realizada.');
 INSERT INTO Registro (id_registro, id_paciente, fecha_registro, observaciones)
 VALUES (3, 3, TO_DATE('2023-07-22', 'YYYY-MM-DD'), 'Ortodoncia en proceso.');
 
-INSERT INTO Estadísticas (id_estadistica, id_doctor, id_tratamiento, fecha, cantidad_realizada)
+INSERT INTO EstadÃ­sticas (id_estadistica, id_doctor, id_tratamiento, fecha, cantidad_realizada)
 VALUES (1, 1, 1, TO_DATE('2023-07-20', 'YYYY-MM-DD'), 5);
-INSERT INTO Estadísticas (id_estadistica, id_doctor, id_tratamiento, fecha, cantidad_realizada)
+INSERT INTO EstadÃ­sticas (id_estadistica, id_doctor, id_tratamiento, fecha, cantidad_realizada)
 VALUES (2, 2, 2, TO_DATE('2023-07-21', 'YYYY-MM-DD'), 3);
-INSERT INTO Estadísticas (id_estadistica, id_doctor, id_tratamiento, fecha, cantidad_realizada)
+INSERT INTO EstadÃ­sticas (id_estadistica, id_doctor, id_tratamiento, fecha, cantidad_realizada)
 VALUES (3, 3, 3, TO_DATE('2023-07-22', 'YYYY-MM-DD'), 2);
 
 INSERT INTO Reembolso (id_reembolso, id_seguro, id_factura, monto)
@@ -282,6 +282,135 @@ BEGIN
     DELETE FROM Doctor
     WHERE id_doctor = p_id_doctor;
 END;
+
+CREATE OR REPLACE PROCEDURE CrearInventario(
+    p_id_inventario IN NUMBER,
+    p_nombre_producto IN VARCHAR2
+    p_cantidad_disponible IN NUMBER,
+    p_costo_unitario IN NUMBER
+) AS
+BEGIN
+    INSERT INTO Inventario (id_inventario, nombre_producto, cantidad_disponible, costo_unitario)
+    VALUES (p_id_inventario, p_nombre_producto, p_cantidad_disponible, p_costo_unitario);
+END;
+
+CREATE OR REPLACE PROCEDURE LeerInventario(
+    p_id_inventario IN NUMBER,
+    p_nombre_producto IN VARCHAR2
+    p_cantidad_disponible IN NUMBER,
+    p_costo_unitario IN NUMBER
+) AS
+BEGIN
+    SELECT id_inventario, nombre_producto, cantidad_disponible, costo_unitario
+    INTO p_id_inventario, p_nombre_producto, p_cantidad_disponible, p_costo_unitario
+    FROM Inventario
+    WHERE id_inventario = p_id_inventario;
+END;
+
+CREATE OR REPLACE PROCEDURE ActualizarInventario(
+    p_id_inventario IN NUMBER,
+    p_nombre_producto IN VARCHAR2
+    p_cantidad_disponible IN NUMBER,
+    p_costo_unitario IN NUMBER
+) AS
+BEGIN
+    UPDATE Inventario
+    SET nombre_producto = p_nombre_producto, cantidad_disponible = p_cantidad_disponible, costo_unitario = p_costo_unitario
+    WHERE id_inventario = p_id_inventario;
+END;
+
+CREATE OR REPLACE PROCEDURE EliminarInventario(
+    p_id_inventario IN NUMBER
+) AS
+BEGIN
+    DELETE FROM Inventario
+    WHERE id_inventario = p_id_inventario;
+END;
+
+CREATE OR REPLACE PROCEDURE CrearSeguro(
+    p_id_seguro IN NUMBER,
+    p_nombre_seguro IN VARCHAR2,
+    p_tipo_cobertura IN VARCHAR2
+ 
+) AS
+BEGIN
+    INSERT INTO Seguro(id_seguro, nombre_seguro, tipo_cobertura)
+    VALUES (p_id_seguro, p_nombre_seguro, p_tipo_cobertura);
+END;
+
+CREATE OR REPLACE PROCEDURE LeerSeguro(
+    p_id_seguro IN NUMBER,
+    p_nombre_seguro IN VARCHAR2,
+    p_tipo_cobertura IN VARCHAR2
+) AS
+BEGIN
+    SELECT id_seguro, nombre_seguro, tipo_cobertura
+    INTO p_id_seguro, p_nombre_seguro, p_tipo_cobertura
+    FROM Seguro
+    WHERE id_seguro = p_id_seguro;
+END;
+
+CREATE OR REPLACE PROCEDURE ActualizarSeguro(
+   p_id_seguro IN NUMBER,
+    p_nombre_seguro IN VARCHAR2,
+    p_tipo_cobertura IN VARCHAR2
+) AS
+BEGIN
+    UPDATE Seguro
+    SET nombre_seguro = p_nombre_seguro, tipo_cobertura = p_tipo_cobertura
+    WHERE id_seguro = p_id_seguro;
+END;
+
+CREATE OR REPLACE PROCEDURE EliminarSeguro(
+    p_id_seguro IN NUMBER
+) AS
+BEGIN
+    DELETE FROM Seguro
+    WHERE id_seguro = p_id_seguro;
+END;
+
+CREATE OR REPLACE PROCEDURE CrearTratamiento(
+    p_id_tratamiento IN NUMBER,
+    p_nombre_tratamiento IN VARCHAR2,
+    p_costo IN NUMBER
+ 
+) AS
+BEGIN
+    INSERT INTO Tratamiento(id_tratamiento, nombre_tratamiento, costo)
+    VALUES (p_id_tratamiento, p_nombre_tratamiento, p_costo);
+END;
+
+CREATE OR REPLACE PROCEDURE LeerTratamiento(
+    p_id_tratamiento IN NUMBER,
+    p_nombre_tratamiento IN VARCHAR2,
+    p_costo IN NUMBER
+) AS
+BEGIN
+    SELECT id_tratamiento, nombre_tratamiento, costo
+    INTO p_id_tratamiento, p_nombre_tratamiento, p_costo
+    FROM Tratamiento
+    WHERE id_tratamiento = p_id_tratamiento;
+END;
+
+CREATE OR REPLACE PROCEDURE ActualizarTratamiento(
+    p_id_tratamiento IN NUMBER,
+    p_nombre_tratamiento IN VARCHAR2,
+    p_costo IN NUMBER
+) AS
+BEGIN
+    UPDATE Tratamiento
+    SET nombre_tratamiento = p_nombre_tratamiento, costo = p_costo
+    WHERE id_tratamiento = p_id_tratamiento;
+END;
+
+CREATE OR REPLACE PROCEDURE EliminarTratamiento(
+    p_id_tratamiento IN NUMBER
+) AS
+BEGIN
+    DELETE FROM Tratamiento
+    WHERE id_tratamiento = p_id_tratamiento;
+END;
+
 
 
 
